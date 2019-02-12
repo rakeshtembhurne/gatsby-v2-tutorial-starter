4a637add4c910d605e08bfd35d583ea05f9d6d12
Rakesh Tembhurne <tj2point0@gmail.com>
2019-02-12 18:50:01 +0530

    Textual changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,8 +24,8 @@ const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
-      <Helmet title={'Home Page'} />
-      <Header title="Home Page">Gatsby Tutorial Starter</Header>
+      <Helmet title={'Anand Sharma'} />
+      <Header title="Anand Sharma"></Header>
       <PostWrapper>
         {edges.map(({ node }) => (
           <PostList
